2e26381ee930002d09ca16fcafc4f57274b92d92
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 14:12:57 +0300

    hi

--- separation/code.js
+++ separation/code.js
@@ -1,1 +1,9 @@
-// Add your alert() here!
+var person = {
+    firstName: "jack",
+    lastName: "bandak",
+    age: 16,
+    favoriteFoods: ["burgers", "pinapple"],
+    
+}
+JSON.stringify(<person>)
+alert(person)
